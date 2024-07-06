ab34923cf95249fa5d7f0b58e137f418ba27cadf
wzc520pyfm <1528857653@qq.com>
2024-07-07 00:17:47 +0800

    fix: fix image dir

--- components/Image.vue
+++ components/Image.vue
@@ -1,6 +1,6 @@
 <template>
-    <NuxtImg :src="`${props.src}`"
-        :placeholder="img(`${props.src}`, { h: 20, w: 20, blur: 16, q: 90 })" fit="cover"
+    <NuxtImg :src="`/${props.src}`"
+        :placeholder="img(`/${props.src}`, { h: 20, w: 20, blur: 16, q: 90 })" fit="cover"
         class="size-60 object-center object-cover cursor-pointer transform-gpu transition-all ease-in-out duration-150 will-change-transform group-hover:scale-102 group-hover:group-odd:rotate-2 group-hover:group-even:-rotate-2 grayscale" alt="
         image-work" @click="showPreviewModal = true" loading="lazy" />
     <UModal v-model="showPreviewModal" fullscreen>
@@ -9,8 +9,8 @@
                 <UButton aria-label="button to close full preview" variant="link" color="white"
                     icon="i-heroicons-x-mark-20-solid" class="ring-1 ring-blue-200" @click="showPreviewModal = false" />
             </div>
-            <NuxtImg :src="`${props.src}`"
-                :placeholder="img(`${props.src}`, { h: 20, w: 20, blur: 16, q: 90 })" fit="cover"
+            <NuxtImg :src="`/${props.src}`"
+                :placeholder="img(`/${props.src}`, { h: 20, w: 20, blur: 16, q: 90 })" fit="cover"
                 class="size-full object-center object-cover" loading="lazy" />
         </div>
     </UModal>
